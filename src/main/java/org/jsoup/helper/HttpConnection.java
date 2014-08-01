79e14e677a17f1954cac556340ffe8722d4332f8
Travis Wellman <tw@traviswellman.com>
2014-08-01 10:31:37 -0700

    Use a regular expression pattern to parse various xml content types
    and simplify the boolean expression to use less nesting, ie just
    checking and not this, and not that, etcetera.

--- src/main/java/org/jsoup/helper/HttpConnection.java
+++ src/main/java/org/jsoup/helper/HttpConnection.java
@@ -19,11 +19,12 @@ import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.X509Certificate;
 import java.util.*;
+import java.util.regex.Pattern;
 import java.util.zip.GZIPInputStream;
 
 /**
  * Implementation of {@link Connection}.
- * @see org.jsoup.Jsoup#connect(String) 
+ * @see org.jsoup.Jsoup#connect(String)
  */
 public class HttpConnection implements Connection {
     public static Connection connect(String url) {
@@ -396,12 +397,12 @@ public class HttpConnection implements Connection {
         public Collection<Connection.KeyVal> data() {
             return data;
         }
-        
+
         public Request parser(Parser parser) {
             this.parser = parser;
             return this;
         }
-        
+
         public Parser parser() {
             return parser;
         }
@@ -427,6 +428,13 @@ public class HttpConnection implements Connection {
         private int numRedirects = 0;
         private Connection.Request req;
 
+        /**
+         * For example {@code application/atom+xml;charset=utf-8}.
+         * Stepping through it: start with {@code "application/"}, follow with word
+         * characters up to a {@code "+xml"}, and then maybe more ({@code .*}).
+         */
+        public static final Pattern xmlContentTypeRxp = Pattern.compile("application/\\w+\\+xml.*");
+
         Response() {
             super();
         }
@@ -439,7 +447,7 @@ public class HttpConnection implements Connection {
                     throw new IOException(String.format("Too many redirects occurred trying to load URL %s", previousResponse.url()));
             }
         }
-        
+
         static Response execute(Connection.Request req) throws IOException {
             return execute(req, null);
         }
@@ -488,12 +496,11 @@ public class HttpConnection implements Connection {
 
                 // check that we can handle the returned content type; if not, abort before fetching it
                 String contentType = res.contentType();
-                if (contentType != null && !req.ignoreContentType()
-                        && (!(contentType.startsWith("text/")
-                            || contentType.startsWith("application/xml")
-                            || contentType.startsWith("application/xhtml+xml")
-                            || (contentType.startsWith("application/") && contentType.endsWith("+xml"))
-                            ))
+                if (contentType != null
+                        && !req.ignoreContentType()
+                        && !contentType.startsWith("text/")
+                        && !contentType.startsWith("application/xml")
+                        && !xmlContentTypeRxp.matcher(contentType).matches()
                         )
                     throw new UnsupportedMimeTypeException("Unhandled content type. Must be text/*, application/xml, or application/xhtml+xml",
                             contentType, req.url().toString());
@@ -677,18 +684,18 @@ public class HttpConnection implements Connection {
             OutputStreamWriter w = new OutputStreamWriter(outputStream, DataUtil.defaultCharset);
             boolean first = true;
             for (Connection.KeyVal keyVal : data) {
-                if (!first) 
+                if (!first)
                     w.append('&');
                 else
                     first = false;
-                
+
                 w.write(URLEncoder.encode(keyVal.key(), DataUtil.defaultCharset));
                 w.write('=');
                 w.write(URLEncoder.encode(keyVal.value(), DataUtil.defaultCharset));
             }
             w.close();
         }
-        
+
         private static String getRequestCookieString(Connection.Request req) {
             StringBuilder sb = new StringBuilder();
             boolean first = true;
@@ -772,6 +779,6 @@ public class HttpConnection implements Connection {
         @Override
         public String toString() {
             return key + "=" + value;
-        }      
+        }
     }
 }
